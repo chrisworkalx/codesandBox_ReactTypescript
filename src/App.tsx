4aaddf6f7ae443a73eca405e8f2c6c31586f9ebc
chrisworkalx <40884918+chrisworkalx@users.noreply.github.com>
2021-09-17 23:12:44 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const App: React.FC = (props: any) => {
+	return (
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+			<p>hello</p>
+		</div>
+	);
+};
+
+export default App;
